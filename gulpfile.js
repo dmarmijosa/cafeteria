ffa8c62ccf27720d001e5cebfc0418bc6eaf2fa8
Danny Armijos <daniel2667@hotmail.es>
2023-08-04 14:13:22 +0200

    proyecto fin

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 const {src, dest, watch, series, parallel} = require("gulp");
+// css y sass
 const sass = require("gulp-sass")(require("sass"));
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+const sourcemaps = require('gulp-sourcemaps');
+const cssnano = require('cssnano');
 // imagenes
 const imagenmin = require('gulp-imagemin');
 const webp = require('gulp-webp');
@@ -14,8 +17,10 @@ const css = (done) => {
     //compilar sass
     // pasos: 1 .identificar archivo, 2 - compilar, 3 -  guardar css
     src("src/scss/app.scss")
+        .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: "expanded"}))
-        .pipe(postcss([autoprefixer()]))
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
         .pipe(dest("build/css"));
     done();
 };
